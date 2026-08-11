06c5660ffed970d2b409f0ab78287aa837d70d5a
Christopher Osthues <christopher.osthues@gmail.com>
2026-08-11 23:50:06 +0200

    refactor: Fix warnings

--- Shelfly.Common/Book.cs
+++ Shelfly.Common/Book.cs
@@ -3,9 +3,9 @@ namespace Shelfly.Common;
 public class Book
 {
     public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public string ISBN { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+    public string ISBN { get; set; } = string.Empty;
     public DateTime PublishDate { get; set; }
 
     public List<Bookmark> Bookmarks { get; set; } = [];
