84c553133835d5ef495f5d99598101bd1f62a28f
Christopher Osthues <christopher.osthues@gmail.com>
2026-08-13 14:40:34 +0200

    test: Set up test projects for API and client app

---
+++ Shelfly.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Shelfly.Api.Tests;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.Fail("");
+    }
+}
---
+++ Shelfly.App.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Shelfly.App.Tests;
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.Fail("");
+    }
+}
