edea5f7fb08501b3200fb80d176bd9e28f875c02
Christopher Osthues <christopher.osthues@gmail.com>
2026-08-14 21:17:40 +0200

    feat: Add test keycloak config

---
+++ Shelfly.Api.Tests/Helpers/KeycloakConfig.cs
@@ -0,0 +1,15 @@
+namespace Shelfly.Api.Tests.Helpers;
+
+internal static class KeycloakConfig
+{
+    internal const string Realm = "test-tracker";
+    internal const int Port = 8083;
+    internal const string KeycloakHostName = "host";
+    internal const string ClientId = "TestTracker";
+    internal const string TestUserName = "Test";
+    internal const string TestEMail = "test@test.com";
+    internal const string TestFirstName = "Test";
+    internal const string TestLastName = "User";
+    internal const string TestPassword = "Test123";
+    internal const string TestClientSecret = "H3ESDjZpmoCiVKCF34S19NnbFu90ETX3";
+}
